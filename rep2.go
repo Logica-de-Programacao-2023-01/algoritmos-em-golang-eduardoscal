7343953d2b35c53ffe3a8d542651d9350eac15aa
eduardoscal <126472349+eduardoscal@users.noreply.github.com>
2023-06-04 18:12:51 -0300

    Update rep2.go

--- rep2.go
+++ rep2.go
@@ -3,22 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var num1, num2, num3 int
-
-	fmt.Println("Digite o primeiro número:")
-	fmt.Scanln(&num1)
-	fmt.Println("Digite o segundo número:")
-	fmt.Scanln(&num2)
-	fmt.Println("Digite o terceiro número:")
-	fmt.Scanln(&num3)
-
-	if num1 < num2 && num1 < num3 {
-		fmt.Println("O menor número é:", num1)
-	} else if num2 < num1 && num2 < num3 {
-		fmt.Println("O menor número é:", num2)
-	} else if num3 < num1 && num3 < num2 {
-		fmt.Println("O menor número é:", num3)
-	} else {
-		fmt.Println("Os números são iguais.")
+	for i := 0; i <= 20; i += 2 {
+		fmt.Println(i)
 	}
 }
