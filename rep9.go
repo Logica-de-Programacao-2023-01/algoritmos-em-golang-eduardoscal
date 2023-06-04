98acecb32604fed1b1170e57c035bb3212e13287
eduardoscal <126472349+eduardoscal@users.noreply.github.com>
2023-06-04 18:14:21 -0300

    Create rep9.go

---
+++ rep9.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		numero   int
+		soma     int
+		quantidade int
+	)
+
+	fmt.Println("Digite os números (digite 0 para encerrar a leitura):")
+
+	for {
+		fmt
