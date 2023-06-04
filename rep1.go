a07f5c9087bf68b7720c8c75d21424b679ae8118
eduardoscal <126472349+eduardoscal@users.noreply.github.com>
2023-06-04 18:12:40 -0300

    Update rep1.go

--- rep1.go
+++ rep1.go
@@ -3,18 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var num1, num2 int
-
-	fmt.Println("Digite o primeiro número:")
-	fmt.Scanln(&num1)
-	fmt.Println("Digite o segundo número:")
-	fmt.Scanln(&num2)
-
-	if num1 > num2 {
-		fmt.Println("O maior número é:", num1)
-	} else if num2 > num1 {
-		fmt.Println("O maior número é:", num2)
-	} else {
-		fmt.Println("Os números são iguais.")
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
 	}
 }
